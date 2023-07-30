720974897d6762ec46739da0695f5646790967d3
juwon5272 <jackson5272@naver.com>
2023-07-30 14:23:55 +0900

    feat : day7-2 map과 refetchQueries

---
+++ pages/07-04-map-boards-delete/index.js
@@ -0,0 +1,64 @@
+import { useQuery, gql, useMutation } from "@apollo/client";
+import styled from "@emotion/styled";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($number: Int!) {
+    deleteBoard(number: $number) {
+      message
+    }
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const Column = styled.div`
+  width: 25%;
+`;
+
+export default function StaticRountedPage() {
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+  const { data } = useQuery(FETCH_BOARDS);
+  console.log(data?.fetchBoards);
+
+  const onClickDelete = async (event) => {
+    await deleteBoard({
+      variables: {
+        number: Number(event.target.id),
+      },
+      refetchQueries: [{ query: FETCH_BOARDS }],
+    });
+  };
+
+  return (
+    <>
+      {data?.fetchBoards.map((el) => (
+        <Row key={el.number}>
+          <Column>
+            <input type="checkbox" />
+          </Column>
+          <Column>{el.number}</Column>
+          <Column>{el.title}</Column>
+          <Column>{el.contents}</Column>
+          <Column>
+            <button id={el.number} onClick={onClickDelete}>
+              삭제
+            </button>
+          </Column>
+        </Row>
+      ))}
+    </>
+  );
+}
